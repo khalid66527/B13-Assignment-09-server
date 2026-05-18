e80ecfab0fa6476002407c8c95ee1b3c3ba0aae7
khalid66527 <khalidhasan678954321@gmail.com>
2026-05-18 17:12:56 +0600

    get a api show all cars

--- index.js
+++ index.js
@@ -24,35 +24,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     await client.connect();
 
     const db = client.db("b13-assignment")
-    const carCollection =db.collection("carListeds")
+    const carCollection = db.collection("carListeds")
+    app.get('/addCar', async (req, res) => {
+      const result = await carCollection.find().toArray()
+      res.json(result)
+    })
 
-    app.post("/addCar", async (req,res)=>{
-
+    app.post("/addCar", async (req, res) => {
       const addCarData = req.body
       console.log(addCarData)
       const result = await carCollection.insertOne(addCarData)
-      res.send(result)
+      
+      res.send({
+        success: true,
+        insertedId: result.insertedId
+      });
     })
+    
 
 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('Server is running fine ! ')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
